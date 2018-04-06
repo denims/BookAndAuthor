ae6f3adcd8b3dd156abac9073e774cfe0d3add5d
Deni Simon <denimallel@gmail.com>
2018-04-06 10:57:26 +0530

    final changes

--- src/main/java/com/deni/test/firstone/controllers/AuthorController.java
+++ src/main/java/com/deni/test/firstone/controllers/AuthorController.java
@@ -1,4 +1,21 @@
 package com.deni.test.firstone.controllers;
 
+
+import com.deni.test.firstone.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AuthorController {
+    @Autowired
+    AuthorRepository authorRepository;
+
+    @RequestMapping("/authors")
+    public String getAuthor(Model model){
+        model.addAttribute("authors",authorRepository.findAll());
+        return "authors";
+    }
+
 }
